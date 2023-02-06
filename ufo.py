00e2b833627813be425fc397dde09e11e383339b
Rahul Karki <124417042+c0ded-human@users.noreply.github.com>
2023-02-06 14:59:05 +0530

    Add files via upload
    ##Summary:
    
    This script is a simple website scanner that checks whether a list of URLs or a single URL contains a specified string. If a URL is found to contain the string, the script prints a message saying "found" and the URL. If a URL does not contain the string, the script prints a message saying "fail" and the URL. The script also provides the option to save the results to an output file and to only show positive results.
    
    ##Usage:
    
    This script is used from the command line and has the following options:
    
    -uf/--url_file: A file containing a list of URLs to be checked.
    
    -u/--single_url: A single URL to check.
    
    -mc/--check_string: The string to check for in the response.
    
    -o/--output: An output file name to save the results.
    
    -p/--positive: Only show positive results (URLs that contain the check_string).
    
    ##Example:
    $ cat dummy-site.txt | qsreplace 'fuzzme' | grep 'fuzzme'  | sort -u > test.txt
    
    $ python script.py -uf test.txt -mc "fuzzme" -o output.txt  -p 
    
    Note: The qsreplace tool is not part of this script and must be installed separately.
    
    The script was developed by a developer named c0ded-human.

---
+++ ufo.py
@@ -0,0 +1,75 @@
+import argparse
+import requests
+import re
+import colorama
+import sys
+
+colorama.init()
+
+def print_name():
+    print(colorama.Fore.RED     + "       ____   ")
+    print(colorama.Fore.RED +     " __ __/ __/__ ")
+    print(colorama.Fore.YELLOW +  "/ // / _// _ \ ")
+    print(colorama.Fore.GREEN +   "\_,_/_/  \___/ ")
+    print(colorama.Fore.MAGENTA + "             dev: +-+ c0ded-human\n")
+
+def check_response(urls, check_string):
+    for url in urls:
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            if re.search(check_string, response.text, re.IGNORECASE):
+                print(f"\033[1;37m[\033[1;32mfound\033[1;37m]\033[0m {url} \033[1;37m[\033[1;32mreflect: {check_string}\033[1;37m]\033[0m")
+                if args.output:
+                    if args.positive:
+                        with open(args.output, "a") as f:
+                            f.write(f"[found] {url} [reflect: {check_string}]\n")
+            else:
+                if not args.positive:
+                    print(f"\033[1;37m[\033[1;31mfail\033[1;37m]\033[0m {url}")
+        except requests.exceptions.RequestException as e:
+            print(f"\033[1;37m[\033[1;94minfo\033[1;37m]\033[0m Exception occurred while processing {url}: {e}")
+            if args.output:
+                with open(args.output, "a") as f:
+                    f.write(f"[info] Exception occurred while processing {url}: {e}\n")
+
+#arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-uf", "--url_file", help="file containing a list of urls to be checked")
+parser.add_argument("-u", "--single_url", help="single URL to check")
+parser.add_argument("-mc", "--check_string", help="string to check for in the response")
+parser.add_argument("-o", "--output", help="output file name to save the results")
+parser.add_argument("-p", "--positive", help="only show positive results (URLs that contains the check_string)", action="store_true")
+args = parser.parse_args()
+
+print_name()
+
+if args.url_file:
+    try:    
+        with open(args.url_file, "r") as f:
+            urls = f.read().splitlines()
+        print("\033[1;37m[\033[1;94minfo\033[1;37m]\033[0m Target loaded: " + str(len(urls)))
+        if args.check_string:
+            print("\033[1;37m[\033[1;94minfo\033[1;37m]\033[0m Match String: \033[1;37m[\033[1;32m{}\033[1;37m]\033[0m".format(args.check_string))
+        if args.output:
+            print("\033[1;37m[\033[1;94minfo\033[1;37m]\033[0m Output Path: {}\n".format(args.output))
+        check_response(urls, args.check_string)
+    except FileNotFoundError as e:
+        print(f"\033[1;37m[\033[1;31merror\033[1;37m]\033[0m The file '{args.url_file}' was not found. {e}")
+    except KeyboardInterrupt:
+        print("\033[1;37m[\033[1;94minfo\033[1;37m]\033[0m Exiting..."),
+    except Exception as e:
+        print(f"\033[1;37m[\033[1;31merror\033[1;37m]\033[0m An error occurred: {e}")
+elif args.single_url:
+    try:
+        if args.check_string:
+            print("\033[1;37m[\033[1;94minfo\033[1;37m]\033[0m Match String: \033[1;37m[\033[1;32m{}\033[1;37m]\033[0m".format(args.check_string))
+        if args.output:
+            print("\033[1;37m[\033[1;94minfo\033[1;37m]\033[0m Output Path: {}\n".format(args.output))
+        check_response([args.single_url], args.check_string)
+    except KeyboardInterrupt:
+        print("\033[1;37m[\033[1;94minfo\033[1;37m]\033[0m Exiting..."),
+    except Exception as e:
+        print(f"\033[1;37m[\033[1;31merror\033[1;37m]\033[0m An error occurred: {e}")
+else:
+    print(f"\033[1;37m[\033[1;31merror\033[1;37m]\033[0m No URL provided. Please provide a URL list or a single URL using the '-uf' or '-u' flags, respectively.")
